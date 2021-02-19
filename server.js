c01f6888390db168e7db159b2293c7884f87bed9
hkanuri <hkanuri@lululemon.com>
2021-02-19 17:04:06 -0600

    server changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000 ;
 connectDB()
 const app = express();
 app.use(express.json())
